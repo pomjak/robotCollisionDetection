fb18db6041d618baf614c303611fa824710d28d6
Simon Cagala <simon.cagala@protonmail.com>
2024-04-25 21:34:11 +0200

    feat(sim): load/save signals

--- include/simulation.h
+++ include/simulation.h
@@ -1,26 +1,38 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <QList>
-
+#include <QGraphicsScene>
 #include "robot.h"
 #include "obstacle.h"
 #include "debug.h"
+#include "json_handler.h"
 
-class Simulation
+using std::shared_ptr;
+
+class Simulation : public QObject
 {
+    Q_OBJECT
 private:
     QList<Robot*> robotList;
     QList<Obstacle*> obstacleList;
+    shared_ptr<QGraphicsScene> scene;
+    JsonHandler json;
     double time;
 
 public:
-    Simulation() : time(0.0) {}
+    Simulation();
     ~Simulation() {}
 
-    QList<Robot*>* getRobotList() { return &robotList; }
-    QList<Obstacle*>* getObstacleList() { return &obstacleList; }
+    QList<Robot*>* getRobotList() const { return new QList<Robot*>(robotList); }
+    QList<Obstacle*>* getObstacleList() const { return new QList<Obstacle*>(obstacleList); }
     void addRobot(Robot* robot) { robotList.push_back(robot); }
     void addObstacle(Obstacle* obstacle) { obstacleList.push_back(obstacle); }
     void printLists();
+    shared_ptr<QGraphicsScene> getScene() const { return scene; }
+
+public slots:
+    void loadLevelLayout();
+    void saveLevelLayout();
 };
