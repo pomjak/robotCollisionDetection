731a3ab8c9e17a2c6c331a2443d484ceb49fe9ad
pomjak <pomjak71@gmail.com>
2024-04-25 11:32:13 +0200

    fix: using debug print instead of cout

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -5,16 +5,16 @@ void Simulation::printLists()
 {
     for ( const auto& obs : obstacleList )
     {
-        std::cout << obs->pos().x() << obs->pos().y() << std::endl;
-        std::cout << obs->getSize() << std::endl;
+        DEBUG << obs->pos().x() << obs->pos().y() ;
+        DEBUG << obs->getSize() ;
     }
 
     for ( const auto& rob : robotList )
     {
-        std::cout << rob->getOrientation() << std::endl;
-        std::cout << rob->pos().x() << rob->pos().y() << std::endl;
-        std::cout << rob->getRotation() << std::endl;
-        std::cout << rob->getSize() << std::endl;
-        std::cout << rob->getSpeed() << std::endl;
+        DEBUG << rob->getOrientation() ;
+        DEBUG << rob->pos().x() << rob->pos().y() ;
+        DEBUG << rob->getRotation() ;
+        DEBUG << rob->getSize() ;
+        DEBUG << rob->getSpeed() ;
     }
 }
