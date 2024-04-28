64e240e9285aecbe4e36ce55d934b8e68b784b73
pomjak <pomjak71@gmail.com>
2024-04-29 00:27:28 +0200

    chore: added header comments to *.cpp files

--- src/robot.cpp
+++ src/robot.cpp
@@ -1,3 +1,13 @@
+/**
+ * \file robot.cpp
+ * \author Pomsar Jakub (xpomsa00@stud.fit.vutbr.cz)
+ * \author Simon Cagala <xcagal00@stud.fit.vutbr.cz>
+ * \version 0.1
+ * \date 2024-04-29
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #include "robot.h"
 
 Robot::Robot()
