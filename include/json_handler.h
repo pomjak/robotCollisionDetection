927c0088116645fa274a45193bbd260539322086
pomjak <pomjak71@gmail.com>
2024-04-28 20:55:10 +0200

    chore(json): wip comments

--- include/json_handler.h
+++ include/json_handler.h
@@ -1,3 +1,14 @@
+/**
+ * \file json_handler.h
+ * \author Pomsar Jakub (xpomsa00@stud.fit.vutbr.cz)
+ * \author Simon Cagala <xcagal00@stud.fit.vutbr.cz>
+ * \brief Header file for JSON handler class, which is being interface between
+ * json files and simulation file
+ * \version 0.1 \date 2024-04-28
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #pragma once
 
 #include <QList>
@@ -6,14 +17,42 @@
 #include "debug.h"
 #include "obstacle.h"
 #include "robot.h"
+
+/**
+ * \brief Interface between simulation and saving/loading objects from JSON
+ *
+ */
 class JsonHandler
 {
   private:
-    QList<Robot *>    *robotList;
-    QList<Obstacle *> *obstacleList;
+    QList<Robot *>
+        *robotList; /* reference on robot list from simulation class */
+    QList<Obstacle *>
+        *obstacleList; /* reference on robot list from simulation class */
 
+    /**
+     * \brief Returns parameters of robot as Json object
+     * 
+     * \param _ Robot to be processed
+     * \return QJsonObject which contains parameters of processed object
+     */
     QJsonObject saveToJson(Robot *_) const;
+
+    /**
+     * \brief Returns parameters of obstacle as Json object
+     *
+     * \param _
+     * \return QJsonObject
+     */
     QJsonObject saveToJson(Obstacle *_) const;
+
+    /**
+     * \brief Get the Json Objects object
+     * 
+     * Collects all json objects from all items and process it into an json array 
+     * 
+     * \return QJsonObject 
+     */
     QJsonObject getJsonObjects();
     void        read(const QJsonObject &json);
     void        addRobot(Robot *robot) { robotList->push_back(robot); }
